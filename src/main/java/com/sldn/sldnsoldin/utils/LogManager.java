ff5b1a84dcc41fd336c6a5a8f9ef858fd18df33f
Soldn <kyryll524@gmail.com>
2025-08-18 21:59:10 +0100

    Update LogManager.java

--- src/main/java/com/sldn/sldnsoldin/utils/LogManager.java
+++ src/main/java/com/sldn/sldnsoldin/utils/LogManager.java
@@ -1,31 +1,40 @@
 package com.sldn.sldnsoldin.utils;
 
-import com.sldn.sldnsoldin.SLDNSoldin;
-import org.bukkit.Bukkit;
-
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 
 public class LogManager {
+    private final File logFile;
 
-    private final SLDNSoldin plugin;
-    private final File file;
-    private final SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    public LogManager(File dataFolder) {
+        this.logFile = new File(dataFolder, "logs.txt");
 
-    public LogManager(SLDNSoldin plugin) {
-        this.plugin = plugin;
-        this.file = new File(plugin.getDataFolder(), "logs.txt");
-        if (!plugin.getDataFolder().exists()) plugin.getDataFolder().mkdirs();
+        try {
+            if (!dataFolder.exists()) {
+                dataFolder.mkdirs();
+            }
+            if (!logFile.exists()) {
+                logFile.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void log(String player, String cat, String message) {
-        String line = "[" + fmt.format(new Date()) + "][" + cat + "][" + player + "] " + message;
-        Bukkit.getConsoleSender().sendMessage(line);
-        try (FileWriter fw = new FileWriter(file, true)) {
-            fw.write(line + System.lineSeparator());
+    public File getLogFile() {
+        return logFile;
+    }
+
+    public void log(String message) {
+        try {
+            Files.write(
+                logFile.toPath(),
+                (message + System.lineSeparator()).getBytes(),
+                StandardOpenOption.CREATE,
+                StandardOpenOption.APPEND
+            );
         } catch (IOException e) {
             e.printStackTrace();
         }
