9b8afba7d7944e3649743c74341d26f080d066fa
Soldn <kyryll524@gmail.com>
2025-08-18 22:10:00 +0100

    Update SLDNSoldin.java

--- src/main/java/com/sldn/sldnsoldin/SLDNSoldin.java
+++ src/main/java/com/sldn/sldnsoldin/SLDNSoldin.java
@@ -3,35 +3,18 @@ package com.sldn.sldnsoldin;
 import com.sldn.sldnsoldin.utils.LogManager;
 import com.sldn.sldnsoldin.utils.PunishManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.io.File;
 
 public class SLDNSoldin extends JavaPlugin {
-
-    private static SLDNSoldin instance;
     private LogManager logManager;
     private PunishManager punishManager;
 
     @Override
     public void onEnable() {
-        instance = this;
+        this.logManager = new LogManager(getDataFolder());
+        this.punishManager = new PunishManager(logManager);
 
-        if (!getDataFolder().exists()) {
-            getDataFolder().mkdirs();
-        }
-
-        logManager = new LogManager(this);
-        punishManager = new PunishManager(this);
-
-        getLogger().info("SLDNSoldin Anticheat enabled!");
-        // TODO: register commands, listeners, checks
-    }
-
-    @Override
-    public void onDisable() {
-        getLogger().info("SLDNSoldin Anticheat disabled!");
-    }
-
-    public static SLDNSoldin getInstance() {
-        return instance;
+        getLogger().info("SLDNSoldin enabled!");
     }
 
     public LogManager getLogManager() {
@@ -40,5 +23,9 @@ public class SLDNSoldin extends JavaPlugin {
 
     public PunishManager getPunishManager() {
         return punishManager;
+    }
+
+    public File getLogFile() {
+        return logManager.getLogFile();
     }
 }
