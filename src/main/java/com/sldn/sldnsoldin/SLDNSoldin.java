e684bb34f45c5e7985ea21c6db2c737933cb68e5
Soldn <kyryll524@gmail.com>
2025-08-18 21:56:09 +0100

    Update SLDNSoldin.java

--- src/main/java/com/sldn/sldnsoldin/SLDNSoldin.java
+++ src/main/java/com/sldn/sldnsoldin/SLDNSoldin.java
@@ -1,37 +1,59 @@
 package com.sldn.sldnsoldin;
 
 import com.sldn.sldnsoldin.commands.CommandManager;
+import com.sldn.sldnsoldin.checks.CombatCheck;
+import com.sldn.sldnsoldin.checks.FlyCheck;
+import com.sldn.sldnsoldin.utils.LogManager;
+import com.sldn.sldnsoldin.utils.PunishManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import java.io.File;
-import java.io.IOException;
 
 public class SLDNSoldin extends JavaPlugin {
-    private File logFile;
+
+    private static SLDNSoldin instance;
+    private LogManager logManager;
+    private PunishManager punishManager;
 
     @Override
     public void onEnable() {
-        File pluginFolder = getDataFolder();
-        if (!pluginFolder.exists()) {
-            pluginFolder.mkdirs();
-        }
-        logFile = new File(pluginFolder, "logs.txt");
-        if (!logFile.exists()) {
-            try { logFile.createNewFile(); } catch (IOException e) { e.printStackTrace(); }
+        instance = this;
+
+        // Готовим папку плагина
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdirs();
         }
 
-        CommandManager cmdManager = new CommandManager(this);
-        getCommand("sldn").setExecutor(cmdManager);
-        getCommand("ban").setExecutor(cmdManager);
-        getCommand("unban").setExecutor(cmdManager);
-        getCommand("tempban").setExecutor(cmdManager);
+        // Инициализируем менеджеры (их используют Checks и PunishManager)
+        this.logManager = new LogManager(this);
+        this.punishManager = new PunishManager(this);
 
-        getLogger().info("SLDNSoldin включен!");
+        // Регистрируем ивенты античита (если у тебя эти классы есть в проекте)
+        getServer().getPluginManager().registerEvents(new FlyCheck(this), this);
+        getServer().getPluginManager().registerEvents(new CombatCheck(this), this);
+
+        // Регистрируем команды
+        CommandManager cmd = new CommandManager(this);
+        getCommand("sldn").setExecutor(cmd);
+        getCommand("ban").setExecutor(cmd);
+        getCommand("unban").setExecutor(cmd);
+        getCommand("tempban").setExecutor(cmd);
+
+        getLogger().info("SLDNSoldin enabled");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("SLDNSoldin выключен!");
+        getLogger().info("SLDNSoldin disabled");
     }
 
-    public File getLogFile() { return logFile; }
+    public static SLDNSoldin get() {
+        return instance;
+    }
+
+    public LogManager getLogManager() {
+        return logManager;
+    }
+
+    public PunishManager getPunishManager() {
+        return punishManager;
+    }
 }
